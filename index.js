86b6ea4572e82b2bf3d4d42353a56fc4fd10b322
Ashraful Islam <theashrafislam@gmail.com>
2025-02-07 18:34:52 +0600

    allover work done

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const app = express();
 require('dotenv').config()
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true
+}));
 app.use(express.json())
+app.use(cookieParser())
 
 
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASS);
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gphdl2n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -30,9 +34,55 @@ async function run() {
         // await client.connect();
 
 
+        // verifyToken
+        const verifyToken = (req, res, next) => {
+            const token = req?.cookies?.token;
+            if (!token) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            jwt.verify(token, process.env.secret, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.user = decoded;
+                next();
+            })
+        }
 
 
+        const userCollection = client.db('jwt-auth').collection('userData');
 
+        app.post('/jwt', async (req, res) => {
+            const { email } = req.body;
+            const token = jwt.sign({
+                email
+            }, process.env.secret, { expiresIn: '1h' });
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false,
+                    sameSite: 'lax'
+                })
+                .send({ success: true })
+        });
+
+        app.post('/logout', async (req, res) => {
+            console.log(req.email);
+            res
+                .clearCookie('token', {
+                    maxAge: 0
+                })
+                .send({ success: true, message: 'hello' })
+        })
+
+
+        app.get('/secret-data', verifyToken, async (req, res) => {
+            if(req?.query?.email !== req?.user?.email){
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            const result = await userCollection.find().toArray();
+            res.send({ messages: "Data get successfully", data: result })
+        })
 
 
         // Send a ping to confirm a successful connection
